c588eb9669617d6f724e6b6722e7ee7b725401d2
jasurtojiobyev <tojiboyevj251@gmail.com>
2023-12-28 10:13:37 +0500

    uy ishi 2

--- homework1.py
+++ homework1.py
@@ -1,12 +1,55 @@
-my_list = [121, 233, 42, 22, 43, 334, 54, 254, 55]
-
-def foyiz():
-    son = 100
-    input1 = int(input("Son kiriting: "))
+def find_and_return_odd_repeating_number(input_list):
+    odd_repeating_number = None
+    counts = {}
     index = 0
-    while index < len(my_list):
-        m = my_list[index] / son * input1
-        print(m)
+
+    while index < len(input_list):
+        current_number = input_list[index]
+        if current_number in counts:
+            counts[current_number] += 1
+        else:
+            counts[current_number] = 1
         index += 1
 
-foyiz()
+    for number, count in counts.items():
+        if count % 2 != 0:
+            odd_repeating_number = number
+            break
+
+    return odd_repeating_number
+
+# Misol ro'yxati
+my_list = [1, 2, 3, 4, 5, 3, 2, 7, 8, 9, 10, 1]
+
+# Ro'yxat ichidagi qaytalanib kelayotgan sonlarni topish
+result = find_and_return_odd_repeating_number(my_list)
+print("Qaytalanib kelayotgan toq son:", result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
